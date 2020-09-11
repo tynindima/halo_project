622695abaff90e227cde059877ca5378d0471acc
tynindima <tynindima@gmail.com>
2020-09-11 12:29:22 +0300

    Updates

---
+++ precache-manifest.cf705f910eafdd5426da17b8670af24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ace8348ec0e8b32a7abca2321084f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26065e22faeafbede713",
+    "url": "/static/css/main.50416dd1.chunk.css"
+  },
+  {
+    "revision": "778ee0bfce81dbea3a12",
+    "url": "/static/js/2.c84117b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c84117b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26065e22faeafbede713",
+    "url": "/static/js/main.a5823813.chunk.js"
+  },
+  {
+    "revision": "4b58324da8f682c23851",
+    "url": "/static/js/runtime-main.47c90a29.js"
+  }
+]);
